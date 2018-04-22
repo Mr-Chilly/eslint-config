df4f7faf049f3cd6bab17da8a85eae884f24e37c
Daniel Chilvers <daniel.chilvers@slimmingworld.co.uk>
2018-04-22 23:26:34 +0100

    add prettier support

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ module.exports = {
     './jsx-a11y',
     'eslint-config-airbnb-base',
     'eslint-config-airbnb-base/rules/strict',
+    'eslint-config-prettier',
+    'eslint-config-prettier/react',
   ].map(require.resolve),
   rules: {
+    'prettier/prettier': ['error', {}],
     'react/jsx-filename-extension': 0,
-    'react/no-unused-prop-types': ['error', {
-      skipShapeProps: true,
-    }],
+    'react/no-unused-prop-types': [
+      'error',
+      {
+        skipShapeProps: true,
+      },
+    ],
     'react/jsx-uses-react': 2,
     'react/jsx-uses-vars': 2,
     'react/react-in-jsx-scope': 2,
@@ -17,29 +23,40 @@ module.exports = {
     'react/forbid-prop-types': 0,
     'no-use-before-define': 0,
     'no-empty-pattern': 0,
-    'no-unused-expressions': ['error', {
-      allowShortCircuit: true,
-      allowTernary: true,
-    }],
-    'class-methods-use-this': ['error', {
-      exceptMethods: [
-        'getChildContext',
-        'componentDidMount',
-        'componentWillMount',
-        'componentWillUnmount',
-        'componentWillReceiveProps',
-        'shouldComponentUpdate',
-        'componentWillUpdate',
-        'componentDidUpdate',
-        'render',
-      ],
-    }],
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies: ['**/test/**/*.js'],
-    }],
-    'no-plusplus': ['error', {
-      allowForLoopAfterthoughts: true,
-    }],
-    'slimmingworld/textComponentEnum': 2,
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+      },
+    ],
+    'class-methods-use-this': [
+      'error',
+      {
+        exceptMethods: [
+          'getChildContext',
+          'componentDidMount',
+          'componentWillMount',
+          'componentWillUnmount',
+          'componentWillReceiveProps',
+          'shouldComponentUpdate',
+          'componentWillUpdate',
+          'componentDidUpdate',
+          'render',
+        ],
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/test/**/*.js'],
+      },
+    ],
+    'no-plusplus': [
+      'error',
+      {
+        allowForLoopAfterthoughts: true,
+      },
+    ],
   },
 };
